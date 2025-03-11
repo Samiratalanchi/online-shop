74f07415d35de380b7ff44facd4ecbdfce83e687
Samira <samira73talanchi@gmail.com>
2025-03-11 12:50:56 +0330

    productgrid items part added in homepage

--- src/components/homePage/productGrid/productGrid.tsx
+++ src/components/homePage/productGrid/productGrid.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import product1 from "../../../images/product1.jpg"
 import product2 from "../../../images/product2.jpg"
 import product3 from "../../../images/product3.jpg"
@@ -6,16 +7,10 @@ import product5 from "../../../images/product5.jpg"
 import product6 from "../../../images/product6.jpg"
 import product7 from "../../../images/product7.jpg"
 import product8 from "../../../images/product8.jpg"
-import product9 from "../../../images/product9.jpg"
-import product10 from "../../../images/product10.jpg"
-import product11 from "../../../images/product11.jpg"
-import product12 from "../../../images/product12.jpg"
-import { useEffect, useState } from "react"
 
-const ProductGrid = ({rows, title}:{rows:number, title:string}) => {
+const FeaturedProduct = ({title}:{title:string}) => {
 
-    const [startPoint , setStartPoint] = useState(0)
-    const [itemsPerRow, setItemsPerRow] = useState(4)
+    const [activeTab, setActiveTab] = useState("New Arrivals")
 
     const products = [
         {name: "Product Name", price: 56000 , badge: "New", pic: product1 },
@@ -26,86 +21,51 @@ const ProductGrid = ({rows, title}:{rows:number, title:string}) => {
         {name: "Product Name", price: 56000 , badge: "Sale", pic: product6 },
         {name: "Product Name", price: 56000 , badge: "New", pic: product7 },
         {name: "Product Name", price: 56000 , badge: "Sale", pic: product8 },
-        {name: "Product Name", price: 56000 , badge: "Sale", pic: product9 },
-        {name: "Product Name", price: 56000 , badge: "New", pic: product10 },
-        {name: "Product Name", price: 56000 , badge: "New", pic: product11 },
-        {name: "Product Name", price: 56000 , badge: "New", pic: product12 }
     ]
-
-    const handlePrev = () => {
-        setStartPoint((prev) => (prev === 0 ? products.length - 1 : prev - 1));
-    };
     
-    const handleNext = () => {
-        setStartPoint((prev) => (prev + 1) % products.length);
-    };
-    
-    const getDisplayedProducts = (startIndex: number, count: number) => {
-        const endIndex = startIndex + count;
-        if (endIndex <= products.length) {
-            return products.slice(startIndex, endIndex);
-        } else {
-            return [...products.slice(startIndex, products.length), ...products.slice(0, endIndex % products.length)];
-        }
-    };
-
-    useEffect(() => {
-        const updateItemsPerRow = () => {
-            if (window.innerWidth < 640) {
-                setItemsPerRow(1);
-            } else if (window.innerWidth < 768) {
-                setItemsPerRow(2);
-            } else if (window.innerWidth < 1024) {
-                setItemsPerRow(3);
-            } else {
-                setItemsPerRow(4);
-            }
-        };
-        updateItemsPerRow();
-        window.addEventListener("resize", updateItemsPerRow);
-        return () => window.removeEventListener("resize", updateItemsPerRow);
-    }, []);
-    
+    const tabs = ["New Arrivals", "Best Seller", "Most View", "Discounts"]
 
     return (
         <div className="my-15 w-full">
-            <div className="text-center font-bold text-2xl">{title}</div>
-
-            <div className="flex flex-col justify-center items-center my-10">
-                <div className="flex flex-row items-center lg:gap-x-3 gap-x-0 w-full justify-center">
-                    <button onClick={handlePrev} className="bg-white text-[10px] text-gray-700 hover:bg-orange-300 hover:text-white cursor-pointer px-2 py-1 text-center border border-gray-300">
-                        <p>p</p>
-                        <p>r</p>
-                        <p>e</p>
-                        <p>v</p>
-                    </button>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:w-full w-4/5 max-w-6xl items-center">
-                        {getDisplayedProducts(startPoint,itemsPerRow).map((item, index) => (
-                            <div key={index} className="flex flex-col gap-y-3 p-3 bg-white shadow-md rounded-lg cursor-pointer hover:shadow-xl">
-                                <div className="relative">
-                                    <img src={item.pic} className="w-full" />
-                                    <div className={`absolute text-[10px] rounded-md py-1 px-2 text-white ${item.badge === "New" ? "bg-amber-300" : "bg-red-400"} left-3 top-3`}>{item.badge}</div>
-                                    <div className="absolute text-sm right-3 top-3">${item.price}</div>
-                                </div>
-                                <h1 className="text-center font-medium">{item.name}</h1>
-                                <button className="relative group text-sm px-2 py-1 cursor-pointer bg-gray-100 border border-gray-300 overflow-hidden">
-                                    Buy now
-                                    <span className="absolute left-[-100%] top-0 bottom-0 bg-amber-500 text-white px-2 py-1 transition-all duration-300 group-hover:left-0 w-full">
-                                        Buy now
-                                    </span>
-                                </button>
+        <div className="text-center font-bold text-2xl">{title}</div>
+        <ul className="list-none text-xs flex flex-row gap-x-5 justify-center my-5 ">
+            {tabs.map((tab) => (
+                <li
+                    key={tab}
+                    onClick={() => setActiveTab(tab)}
+                    className={`cursor-pointer p-1 pb-2 ${
+                        activeTab === tab
+                            ? "text-amber-400 border-b-2 border-amber-400"
+                            : "text-gray-600"
+                    }`}
+                >
+                    {tab}
+                </li>
+            ))}
+        </ul>
+        <div className="flex flex-col justify-center items-center my-10">
+            <div className="flex flex-row items-center lg:gap-x-3 gap-x-0 w-full justify-center">
+                <div className={`grid gap-5 md:w-full w-4/5 max-w-6xl items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4`}>
+                    {products.map((item, index) => (
+                        <div key={index} className="flex flex-col gap-y-3 p-3 bg-white shadow-md rounded-lg cursor-pointer hover:shadow-xl">
+                            <div className="relative">
+                                <img src={item.pic} className="w-full" />
+                                <div className={`absolute text-[10px] rounded-md py-1 px-2 text-white ${item.badge === "New" ? "bg-amber-300" : "bg-red-400"} left-3 top-3`}>{item.badge}</div>
+                                <div className="absolute text-sm right-3 top-3">${item.price}</div>
                             </div>
-                        ))}
-                    </div>
-                    <button onClick={handleNext} className="bg-white text-[10px] text-gray-700 hover:bg-orange-300 hover:text-white cursor-pointer px-2 py-1 text-center border border-gray-300">
-                        <p>n</p>
-                        <p>e</p>
-                        <p>x</p>
-                        <p>t</p>
-                    </button>
+                            <h1 className="text-center font-medium">{item.name}</h1>
+                            <button className="relative group text-sm px-2 py-1 cursor-pointer bg-gray-100 border border-gray-300 overflow-hidden">
+                                Buy now
+                                <span className="absolute left-[-100%] top-0 bottom-0 bg-amber-500 text-white px-2 py-1 transition-all duration-300 group-hover:left-0 w-full">
+                                    Buy now
+                                </span>
+                            </button>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
+    </div>
     );
 }
-export default ProductGrid;
+export default FeaturedProduct;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,9 @@
 import Slider from "../../components/homePage/slider/slider"
 import HeaderLayout from "../../components/headerLayout/headerLayout"
 import Banner from "../../components/homePage/banner/banner"
-import ProductGrid from "../../components/homePage/productGrid/productGrid"
+import FeaturedProduct from "../../components/homePage/featuredProduct/featuredProduct"
 import DiscountBanner from "../../components/homePage/discountBanner/discountBanner"
+import ProductGrid from "../../components/homePage/productGrid/productGrid"
 
 const Home = () => {
     return (
@@ -10,8 +11,9 @@ const Home = () => {
             <main>
                 <Slider />
                 <Banner />
-                <ProductGrid rows={1} title="Featured Products" />
+                <FeaturedProduct title="Featured Products" />
                 <DiscountBanner />
+                <ProductGrid title="Purchase Online" />
             </main>
         </HeaderLayout>
         
